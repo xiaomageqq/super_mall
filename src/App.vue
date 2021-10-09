a8cbc32d15cd1b1d349fa07ba2a56df725917dc3
qian.gao123 <357554941@qq.com>
2021-10-09 08:52:05 +0800

    升级版本

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,28 @@
 <template>
   <div id="app">
-    <!-- {{full}}{{full}}{{full}}
-    <input v-model="full" ><br>
-    <input v-model="first" > <br>
-    <input v-model="second" > 
-    --
-    {{fullt()}}
-    {{fullt()}}
-    {{fullt()}}
-    {{fullt()}} -->
-    {{perName}}
-    <input v-model="first" > <br>
-    <input v-model="per.name">
-    <main-tab-bar mm="11"></main-tab-bar>
-    <router-view/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import MainTabBar from './components/MainTabBar'
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
   name: 'App',
-
-  data(){
-    return{
-      first:'美女',
-      second:'姐姐',
-      per:{
-        name:'倩倩',
-        age:'18'
-      }
-    }
-  },
-  watch:{
-    first(newVal,oldVal){
-      console.log('newVal',newVal)
-      console.log('oldVal',oldVal)
-    },
-    per:{
-      handler(oldVal,newVal){
-        console.log('newVal',newVal)
-        console.log('oldVal',oldVal)
-      },
-      deep:true,
-    },
-    'per.name':function(newVal,oldVal){
-      console.log('newVal->',newVal)
-      console.log('oldVal->',oldVal)
-    },
-    perName(){
-      console.log('name改变了')
-    }
-  },
-  computed:{
-    perName:function(){
-      return this.per.name
-    }
-  },
-  // computed:{
-  //   full:{
-  //     get(){
-  //       console.log('computed')
-  //       return this.first + ' ' + this.second
-  //     },
-  //     set(val){
-  //       let names = val.split(' ')
-  //       this.first = names[0]
-  //       this.second = names[1]
-  //     }
-  //   }
-  // },
-  // methods:{
-  //   fullt(){
-  //     console.log('method')
-  //     return this.first + ' ' + this.second
-  //   }
-  // },
-
   components: {
-    MainTabBar,
+    HelloWorld
   }
 }
 </script>
 
 <style>
-@import "./assets/css/base.css";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
-
 </style>
